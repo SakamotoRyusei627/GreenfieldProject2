5f3a814cb414758dc37e51a8c86233e8edfe6a8a
Vermouth117 <e153jsb7eg@gmail.com>
2023-06-03 16:50:45 +0900

    finish

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -2,24 +2,19 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: { directory: "./data/seeds" },
   development: {
     client: "pg",
     connection: {
       database: "gf",
       user: "user",
     },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: { directory: "./data/seeds" },
   },
-
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: { directory: "./data/seeds" },
   },
 };
