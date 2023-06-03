3138a4a970b4306d146f5c715c1a2c95f81445da
Vermouth117 <e153jsb7eg@gmail.com>
2023-06-03 17:54:57 +0900

    port

--- src/component/common/Main.tsx
+++ src/component/common/Main.tsx
@@ -154,7 +154,7 @@ const Main = () => {
   useEffect(() => {
     const getData = async () => {
       const foodList = await fetch(
-        "http://localhost:3333/food/sazaezamasu"
+        "https://vermouth.onrender.com/food/sazaezamasu"
       ).then((e) => e.json());
       setFoodList(foodList);
       const cookList = await fetch(
