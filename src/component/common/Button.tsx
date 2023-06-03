291ae598dc2913dd23e03c2e8fe01363ba89d44b
Vermouth117 <e153jsb7eg@gmail.com>
2023-06-03 20:56:17 +0900

    url

--- src/component/common/Button.tsx
+++ src/component/common/Button.tsx
@@ -127,8 +127,8 @@ const Button: React.FC<props> = ({ foodList, setFoodList, buyFood }) => {
                         // console.log(formButtonRef.current.click());
                         console.log("このデータを送る", resistFood);
                         const fetchData = await fetch(
-                          // `/food/sazaezamasu`,
-                          `http://localhost:3333/food/sazaezamasu`,
+                          `/food/sazaezamasu`,
+                          // `http://localhost:3333/food/sazaezamasu`,
                           {
                             method: "POST",
                             headers: {
@@ -174,8 +174,8 @@ const Button: React.FC<props> = ({ foodList, setFoodList, buyFood }) => {
                           arrFoods: result,
                         };
                         const fetchData = await fetch(
-                          // `/propose`,
-                          `http://localhost:3333/propose`,
+                          `/propose`,
+                          // `http://localhost:3333/propose`,
                           {
                             method: "POST",
                             headers: {
